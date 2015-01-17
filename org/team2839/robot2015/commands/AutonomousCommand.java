4d4dac5bb5f5a7ccbe55bdf30cf113f376ce30a2
team2839 <robotics2839@gmail.com>
2015-01-17 08:30:53 -0800

    Basic autonomous state machine framework

--- org/team2839/robot2015/commands/AutonomousCommand.java
+++ org/team2839/robot2015/commands/AutonomousCommand.java
@@ -1,6 +1,8 @@
 package org.team2839.robot2015.commands;
 
-import org.team2839.robot2015.states.AutonomousState;
+//import org.team2839.robot2015.states.AutonomousState;
+
+import org.team2839.robot2015.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
 
@@ -9,15 +11,26 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class AutonomousCommand extends Command {
 	
+	public enum AutonomousState {
+		START, FINISHED
+	}
+	
 	private AutonomousState state;
 
 	public AutonomousCommand() {
-		// Use requires() here to declare subsystem dependencies
-		// eg. requires(chassis);
+		requires(Robot.lFSPIDSubsystem);
+		requires(Robot.rFSPIDSubsystem);
+		requires(Robot.rRSPIDSubsystem);
+		requires(Robot.lRSPIDSubsystem);
+		requires(Robot.lFDPIDSubsystem);
+		requires(Robot.rFDPIDSubsystem);
+		requires(Robot.rRDPIDSubsystem);
+		requires(Robot.lRDPIDSubsystem);
 	}
 
 	// Called just before this Command runs the first time
 	protected void initialize() {
+		state = AutonomousState.START;
 	}
 
 	// Called repeatedly when this Command is scheduled to run
@@ -27,8 +40,7 @@ public class AutonomousCommand extends Command {
 
 	// Make this return false when this Command no longer needs to run execute()
 	protected boolean isFinished() {
-		// return state.FINISHED (however you check this)
-		return false;
+		return state.equals(AutonomousState.FINISHED);
 	}
 
 	// Called once after isFinished returns true
--- org/team2839/robot2015/states/AutonomousState.java
+++
@@ -1,5 +0,0 @@
-package org.team2839.robot2015.states;
-
-public enum AutonomousState {
-	FINISHED
-}
